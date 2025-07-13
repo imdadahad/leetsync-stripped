d30b3b806440445aab731e6b594e62e701225632
Imdad Ahad <imdadahad1@gmail.com>
2025-07-13 13:31:15 +0100

    Filter submissions in past 2 hours or since last solved and submit to Github

--- src/background.ts
+++ src/background.ts
@@ -1,79 +1,108 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type === 'set-fire-icon') {
-    //set icon to fire then back to normal after 2 second
+import GithubHandler from "./handlers/GithubHandler"
+import LeetCodeHandler from "./handlers/LeetCodeHandler"
+import { RestSubmission } from "./types/Submission"
 
-    chrome.action.setIcon(
-      {
-        path: '../../icon-fire-96x96.gif',
-      },
-      () => {
-        setTimeout(() => {
-          chrome.action.setIcon({
-            path: '../../logo96.png',
-          });
-        }, 5000);
-      },
-    );
-  }
-  /* Will be used if we want to get messages from content scripts to background script */
-  sendResponse({ status: 'OK' });
-});
-chrome.cookies.get({ name: 'LEETCODE_SESSION', url: 'https://leetcode.com/' }, function (cookie) {
-  if (!cookie) return;
-  chrome.storage.sync.set({ leetcode_session: cookie.value }, () => {
-    console.log(`Leetcode Synced Successfully`);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === "process-submissions") {
+        processSubmissionsInBackground(request.submissions)
+        sendResponse({ status: "started" })
+    }
   });
-});
+
+  async function processSubmissionsInBackground(submissions: RestSubmission[]) {
+    for (const submission of submissions) {
+        console.log(`attempting to submit: ${submission}`)
+      try {
+            const leetcodeHandler = new LeetCodeHandler()
+            const github = new GithubHandler()
+            const details = await leetcodeHandler.getSubmission(submission.title_slug)
+            if (details) {
+                console.log(`details: ${details}`)
+                await github.submit(details)
+            }
+            setTimeout(() => 500)
+        } catch (e) {
+            console.error("Error processing submission:", e)
+        }
+    }
+  }
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.type === "set-fire-icon") {
+        //set icon to fire then back to normal after 2 second
+
+        chrome.action.setIcon(
+            {
+                path: "../../icon-fire-96x96.gif",
+            },
+            () => {
+                setTimeout(() => {
+                    chrome.action.setIcon({
+                        path: "../../logo96.png",
+                    })
+                }, 5000)
+            },
+        )
+    }
+    /* Will be used if we want to get messages from content scripts to background script */
+    sendResponse({ status: "OK" })
+})
+chrome.cookies.get({ name: "LEETCODE_SESSION", url: "https://leetcode.com/" }, function (cookie) {
+    if (!cookie) return
+    chrome.storage.sync.set({ leetcode_session: cookie.value }, () => {
+        console.log(`Leetcode Synced Successfully`)
+    })
+})
 
 chrome.cookies.onChanged.addListener(function (info) {
-  const { cookie } = info;
-  //get LEETCODE_SESSION cookie
-  if (cookie.name === 'LEETCODE_SESSION') {
-    //save cookie value to local storage
-    chrome.storage.sync.set({ leetcode_session: cookie?.value || null }, () => {
-      console.log(`Leetcode Re-Synced Successfully`);
-    });
-  }
-});
+    const { cookie } = info
+    //get LEETCODE_SESSION cookie
+    if (cookie.name === "LEETCODE_SESSION") {
+        //save cookie value to local storage
+        chrome.storage.sync.set({ leetcode_session: cookie?.value || null }, () => {
+            console.log(`Leetcode Re-Synced Successfully`)
+        })
+    }
+})
 chrome.storage.sync.onChanged.addListener((changes) => {
-  console.log(`🚀 ~ file: background.ts:68 ~ changes:`, JSON.stringify(changes, null, 2));
-});
+    console.log(`🚀 ~ file: background.ts:68 ~ changes:`, JSON.stringify(changes, null, 2))
+})
 
 export const sendMessageToContentScript = (type: string, data: any) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    if (!tabs.length || !tabs[0].id) return;
-    chrome.tabs.sendMessage(tabs[0].id, { type, data }, function (response) {
-      if (chrome.runtime.lastError) {
-        console.log(chrome.runtime.lastError.message);
-        // Handle the error here
-        return;
-      }
-      console.log(`✅ Acknowledged`, response);
-    });
-  });
-};
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (!tabs.length || !tabs[0].id) return
+        chrome.tabs.sendMessage(tabs[0].id, { type, data }, function (response) {
+            if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError.message)
+                // Handle the error here
+                return
+            }
+            console.log(`✅ Acknowledged`, response)
+        })
+    })
+}
 
 // Listen for submit request
 chrome.webRequest.onCompleted.addListener(
-  (details: chrome.webRequest.WebResponseCacheDetails) => {
-    // Check if it's a POST request to submit the code
-    if (
-      details.method === 'POST' &&
-      details.url.startsWith('https://leetcode.com/problems/') &&
-      details.url.includes('/submit/')
-    ) {
-      const questionSlug = details.url.match(/\/problems\/(.*)\/submit/)?.[1] ?? null;
-      if (!questionSlug) return;
-      // Wait 5 secs to complete the checks
-      // Send a message to the content script to get the submission
-      setTimeout(() => {
-        sendMessageToContentScript('get-submission', { questionSlug });
-      }, 5000);
-    }
-  },
-  {
-    urls: ['https://leetcode.com/problems/*/submit/'],
-    types: ['xmlhttprequest'],
-  },
-);
-export {};
+    (details: chrome.webRequest.WebResponseCacheDetails) => {
+        // Check if it's a POST request to submit the code
+        if (
+            details.method === "POST" &&
+            details.url.startsWith("https://leetcode.com/problems/") &&
+            details.url.includes("/submit/")
+        ) {
+            const questionSlug = details.url.match(/\/problems\/(.*)\/submit/)?.[1] ?? null
+            if (!questionSlug) return
+            // Wait 5 secs to complete the checks
+            // Send a message to the content script to get the submission
+            setTimeout(() => {
+                sendMessageToContentScript("get-submission", { questionSlug })
+            }, 5000)
+        }
+    },
+    {
+        urls: ["https://leetcode.com/problems/*/submit/"],
+        types: ["xmlhttprequest"],
+    },
+)
+
